da19aac28cb2be55d602ff4769a82d7060a6be99
Sergii Repin <sergii.repin@w3.co>
2015-10-03 16:11:40 +0300

    add hello route

--- src/app.php
+++ src/app.php
@@ -11,14 +11,7 @@ $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
 $routes->add('hello', new Routing\Route('/hello/{name}', array(
     'name' => 'World',
     '_controller' => function ($request) {
-        // $foo will be available in the template
-        $request->attributes->set('foo', 'bar');
-
         $response = render_template($request);
-
-        // change some header
-        $response->headers->set('Content-Type', 'text/plain');
-
         return $response;
     }
 )));
