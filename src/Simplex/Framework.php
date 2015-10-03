201adaa6098e17144e143276658cbf720310d21f
Sergii Repin <sergii.repin@w3.co>
2015-10-03 14:58:26 +0300

    add EventDispatcher to framework

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -9,17 +9,20 @@ use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 
 class Framework
 {
     protected $matcher;
     protected $resolver;
+    protected $dispatcher;
 
     public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $resolver)
     {
         $this->matcher = $matcher;
         $this->resolver = $resolver;
+        $this->dispatcher = $dispatcher;
     }
 
     public function handle(Request $request)
@@ -36,5 +39,10 @@ class Framework
         } catch (\Exception $e){
             return new Response('An error occurred', 500);
         }
+
+        // dispatch a response event
+        $this->dispatcher->dispatch('response', new ResponseEvent($response, $request))
+
+        return $response;
     }
 }
