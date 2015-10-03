7737d161c87dbb7a03b83df2be44e5b8a71c74ed
Sergii Repin <sergii.repin@w3.co>
2015-10-03 13:50:10 +0300

    add new route

--- src/app.php
+++ src/app.php
@@ -8,4 +8,19 @@ $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 )));
 
+$routes->add('hello', new Routing\Route('/hello/{name}', array(
+    'name' => 'World',
+    '_controller' => function ($request) {
+        // $foo will be available in the template
+        $request->attributes->set('foo', 'bar');
+
+        $response = render_template($request);
+
+        // change some header
+        $response->headers->set('Content-Type', 'text/plain');
+
+        return $response;
+    }
+)));
+
 return $routes;
